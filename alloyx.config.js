90b2807950104c445f061ab438a5ad0fcca4b10a
NekoSuneVR <Chisdeal2014@hotmail.com>
2024-03-13 23:04:49 +0000

    Update alloyx.config.js

--- alloyx.config.js
+++ alloyx.config.js
@@ -55,7 +55,7 @@ var ipfsLinks = ENV.ipfsLinks
 const bidRate = ENV.BIDRATE || 2500 //
 
 //HIVE CONFIGS
-var startURL = ENV.STARTURL || "https://hive-api.dlux.io/ipfs/";
+var startURL = ENV.STARTURL || "https://hive-api.dlux.io/";
 var clientURL = ENV.APIURL || "https://hive-api.dlux.io/";
 const clients = ENV.clients
   ? ENV.clients.split(" ")
